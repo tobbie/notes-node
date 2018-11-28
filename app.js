a0ad5e5bceb28930458e8f539ec19aa453085064
tobbie <tobi.akinseye@gmail.com>
2018-11-28 13:36:09 +0100

    removed un-needed code

--- app.js
+++ app.js
@@ -1,22 +1,15 @@
 console.log('starting app.js');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+
 const notes = require('./notes.js');
 
-var user = os.userInfo();
-var res = notes.add(10, -2);
-console.log(res);
 
-console.log(_.isString(true));
-console.log(_.isString('Tobi'));
 
-// fs.appendFile('greetings.txt',`\nWelcome to notes app ${user.username}\n`, (err) =>{
-//      if(err)
-//      {
-//          console.log('unable to write file');
-//         throw err;
-//      }   
-// });
+
+
+
+
+
 
--- notes.js
+++ notes.js
@@ -5,7 +5,3 @@ module.exports.addNote = () =>{
     console.log('calling add  note');  
     return "New Note";
 }
-module.exports.add = (firstVal, secondVal) => {
-    console.log('calling add');  
-    return firstVal + secondVal;
-}
